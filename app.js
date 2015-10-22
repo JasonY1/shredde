31f94659ee593d4c4c4df8023ba6e691c959c2c7
JasonY1 <jasonh.yi@gmail.com>
2015-10-21 20:09:51 -0700

    Express: Created first route

--- app.js
+++ app.js
@@ -7,10 +7,24 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var swoleRouter = express.Router();
+
+swoleRouter.route('/Swole')
+.get(function(req,res){
+  var responseJson = { hello: "This is it"};
+  
+  res.json(responseJson);
+});
+
+app.use('/', swoleRouter);
+
+
+
+
 app.get('/', function(req, res){
   res.send('Shit is live');
 });
 
 app.listen(port, function(){
-  console.log('running on port: ' + port);
+  console.log('Gulp is running on PORT: ' + port);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 // Created on 10/21/2015 by Jason Yi
 // Last modified on 10/21/2015 by Jason Yi
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon');
 
-
+gulp.task('default', function(){
+  nodemon({
+    script: 'app.js',
+    ext: 'js',
+    env: {
+      PORT:8000
+    },
+    ignore: ['./node_modules/**']
+  })
+  .on('restart', function(){
+    console.log('Restarting');
+  });
+});
